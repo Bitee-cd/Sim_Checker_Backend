133fad1162fc8856678cade745e9664651b6e0c0
Bitee-cd <Calebduniya45@gmail.com>
2022-10-26 04:27:17 +0100

    restructure

--- sim_checker/sim_checker/settings.py
+++ sim_checker/sim_checker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-81isp#z!-d1(t1s4b9!2rrt!_p#z4u2bbwzmx%2r4o+&xra9au'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
