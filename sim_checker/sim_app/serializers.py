fc7f604e1c2fcd1cdd3490a19a1a36ff992ffcf5
Bitee-cd <Calebduniya45@gmail.com>
2022-10-27 18:00:14 +0100

    foreign key nesting

--- sim_checker/sim_app/serializers.py
+++ sim_checker/sim_app/serializers.py
@@ -1,13 +1,18 @@
 from rest_framework import serializers
-from sim_app.models import PhoneNumber
+from sim_app.models import PhoneNumber,SimNetwork
 
 
+
+
+
+class SimSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=SimNetwork
+        fields = ('id','name')
 
 class SimAppSerializer(serializers.ModelSerializer):
+    sim=SimSerializer(many=False)
     class Meta:
-        fields=(
-            'id',
-            'phone_number',
-            'sim_network'
-        )
         model=PhoneNumber
+        fields = ('id','phone_number','sim',)
+
