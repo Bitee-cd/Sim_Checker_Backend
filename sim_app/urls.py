3f51c66e382d55964d6c9fb3eae8501b83fd3856
Bitee-cd <Calebduniya45@gmail.com>
2022-11-09 15:40:03 +0100

    added find functionality

--- sim_app/urls.py
+++ sim_app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.ListSimApp.as_view()),
+    path('',views.ListSim.as_view()),
+    path('sim/',views.ListPhoneNumber.as_view()),
      path('<int:pk>/', views.DetailSimApp.as_view()),
+     path('find/<str:number_>/', views.FindSim.as_view()),
 ]
 
--- sim_app/views.py
+++ sim_app/views.py
@@ -1,12 +1,28 @@
 from rest_framework import generics
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from sim_app.models import PhoneNumber
+from sim_app.models import SimNetwork
 from sim_app.serializers import SimAppSerializer
+from sim_app.serializers import SimSerializer
 
-class ListSimApp(generics.ListCreateAPIView):
+class ListSim(APIView):
+    def get(self,request):
+        sims = SimNetwork.objects.all()
+        serializer=SimSerializer(sims,many=True)
+        return Response(serializer.data)
+
+class ListPhoneNumber(generics.ListCreateAPIView):
     queryset=PhoneNumber.objects.all()
     serializer_class=SimAppSerializer
 
 class DetailSimApp(generics.RetrieveUpdateDestroyAPIView):
     queryset=PhoneNumber.objects.all()
     serializer_class=SimAppSerializer
+
+class FindSim(APIView):
+    def get(self,request, number_):
+        number = PhoneNumber.objects.filter(phone_number=number_)
+        serializer = SimAppSerializer(number, many=True)
+        return Response(serializer.data)
+
