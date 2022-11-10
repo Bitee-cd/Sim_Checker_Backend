c87df2bbe8ede04ee89c73a8c63d92af5bf21b17
Bitee-cd <Calebduniya45@gmail.com>
2022-11-10 20:12:50 +0100

    response error 404

--- sim_app/views.py
+++ sim_app/views.py
@@ -5,6 +5,7 @@ from sim_app.models import PhoneNumber
 from sim_app.models import SimNetwork
 from sim_app.serializers import SimAppSerializer
 from sim_app.serializers import SimSerializer
+from rest_framework import status
 
 class ListSim(APIView):
     def get(self,request):
@@ -24,5 +25,14 @@ class FindSim(APIView):
     def get(self,request, number_):
         number = PhoneNumber.objects.filter(phone_number=number_)
         serializer = SimAppSerializer(number, many=True)
-        return Response(serializer.data)
+        print(serializer.data)
+        if (len(serializer.data)>0):
+            data=serializer.data
+            message="Success",
+            return Response({"data":data,"message":message,}, status=status.HTTP_200_OK)
+        else:
+            data=[]
+            message="Sorry, data not found in database"
+            return Response({"message":message}, status=status.HTTP_404_NOT_FOUND)
+       
 
